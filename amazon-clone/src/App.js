82b69810aa76647111fd210addbf83a210dc6068
Cnaf254 <133096126+Cnaf254@users.noreply.github.com>
2024-02-05 12:57:08 -1000

    Header,Carousel and Category

---
+++ amazon-clone/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Header from "./Components/Header/Header"
+import Carousel from "./Components/Carousel/CarouselEffect"
+import Category from './Components/Category/Category';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Carousel />
+      <Category />
+    </div>
+  );
+}
+
+export default App;
